40f02c83b94ede94cc54a334013486358cfbca10
Darknees1997 <38507376+Darknees1997@users.noreply.github.com>
2019-07-02 18:58:33 -0400

    Update app.component.ts

--- VJuegosF/src/app/app.component.ts
+++ VJuegosF/src/app/app.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-import { NavController } from "@ionic/angular";
 
 @Component({
   selector: 'app-root',
@@ -13,8 +12,7 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar,
-    public navcrl: NavController
+    private statusBar: StatusBar
   ) {
     this.initializeApp();
   }
@@ -24,25 +22,5 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
-  }
-  lol(){
-    this.navcrl.navigateForward("page1");
-    
-  }
-  mk(){
-    this.navcrl.navigateForward("page2");
-    
-  }
-  ff(){
-    this.navcrl.navigateForward("page3");
-    
-  }
-  ps5(){
-    this.navcrl.navigateForward("page4");
-    
-  }
-  jf(){
-    this.navcrl.navigateForward("page5");
-    
   }
 }
